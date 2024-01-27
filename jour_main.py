a1868f399cf3098069cd1cf79ba92f1dbd4d364e
Ed-20 <ednet@mail.ru>
2024-01-27 13:09:06 +0300

    rrr

--- jour_main.py
+++ jour_main.py
@@ -1,12 +1,18 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
 from main import Ui_MainWindow
+from PySide6.QtSql import QSqlDatabase
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        DB = QSqlDatabase.addDatabase('QSQLITE')
+        DB.setDatabaseName("jourbd.sqlite")
+        DB.open()
+
 
 
 if __name__ == '__main__':
