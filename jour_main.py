86776f20ed33b91c70c9cdf03d2aba687f28fca5
Leonid Gnezdilov <comdir@infonix.info>
2024-01-29 14:16:18 +0300

    upd

--- jour_main.py
+++ jour_main.py
@@ -1,7 +1,8 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow,QHeaderView
+from PySide6.QtWidgets import QApplication, QMainWindow,QHeaderView,QTableWidget,QTableWidgetItem
 from main import Ui_MainWindow
 from PySide6.QtSql import QSqlDatabase, QSqlTableModel, QSqlQuery
+#from PySide6 import QtWidgets
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -13,31 +14,54 @@ class MainWindow(QMainWindow):
         self.ui.pushButton_Change.clicked.connect(self.changeRec)
         self.ui.pushButton_Del.clicked.connect(self.delRec)
 
+
         DB = QSqlDatabase.addDatabase('QSQLITE')
         DB.setDatabaseName("jourbd.sqlite")
         DB.open()
 
+        self.updateWidg()
+
+
+        
+    def updateWidg(self):
+        query = QSqlQuery()
+        qcount = QSqlQuery()
+        qcount.exec("SELECT COUNT(*) FROM jtab;")
+        qcount.first()
+        query.exec("SELECT * FROM jtab ;")
+        #query.first()
+        r=0
+        self.ui.tableWidget.setRowCount(int(qcount.value(0))-1)
+        while query.next():
+            #self.ui.tableWidget.setItem(r, 0,  QTableWidgetItem(f'line_{r}'))
+            self.ui.tableWidget.setItem(r, 0,  QTableWidgetItem(str(query.value(0))))
+            self.ui.tableWidget.setItem(r, 1,  QTableWidgetItem(str(query.value(1))))
+            self.ui.tableWidget.setItem(r, 2,  QTableWidgetItem(str(query.value(2))))
+            self.ui.tableWidget.setItem(r, 3,  QTableWidgetItem(str(query.value(3))))
+            self.ui.tableWidget.setItem(r, 4,  QTableWidgetItem(str(query.value(4))))
+            self.ui.tableWidget.setItem(r, 5,  QTableWidgetItem(str(query.value(5))))
+            self.ui.tableWidget.setItem(r, 6,  QTableWidgetItem(str(query.value(6))))
+            self.ui.tableWidget.setItem(r, 7,  QTableWidgetItem(str(query.value(8))))
+            r+=1
+            if str(query.value(7)) == True: #оплачено
+                #self.ui.tableWidget.setPalette #Должно поменять цвет строки
+                pass
+            if str(query.value(7)) == True: #выдано
+                #self.ui.tableWidget. #Должно поменять цвет строки
+                pass  
+
+
+        self.ui.tableWidget.resizeColumnsToContents()
+
+    def findRec(self):
+        query = QSqlQuery()
+        query.exec("SELECT *  FROM jtab WHERE npp = 1 ;")
         jmodel=QSqlTableModel()
         jmodel.setTable("jtab")
         self.ui.tableView.setModel(jmodel)
+        jmodel.setFilter()
         jmodel.select()
-
-        header = self.ui.tableView.horizontalHeader()       
-        header.setSectionResizeMode(0, QHeaderView.ResizeMode.ResizeToContents)
-        header.setSectionResizeMode(1, QHeaderView.ResizeMode.ResizeToContents)
-        header.setSectionResizeMode(2, QHeaderView.ResizeMode.Stretch)
-        header.setSectionResizeMode(3, QHeaderView.ResizeMode.Stretch)
-        header.setSectionResizeMode(4, QHeaderView.ResizeMode.Stretch)
-        header.setSectionResizeMode(5, QHeaderView.ResizeMode.ResizeToContents)
-        header.setSectionResizeMode(6, QHeaderView.ResizeMode.ResizeToContents)
-        header.setSectionResizeMode(7, QHeaderView.ResizeMode.ResizeToContents)
-        header.setSectionResizeMode(8, QHeaderView.ResizeMode.ResizeToContents)
-
-        
-
-
-    def findRec(self):
-        pass
+        #pass
 
     def addRec(self):
         adq = QSqlQuery()
@@ -52,10 +76,11 @@ class MainWindow(QMainWindow):
         pass  
 
     def formUpd(self):
-        jmod=QSqlTableModel()
-        jmod.setTable("jtab")
-        self.ui.tableView.setModel(jmod)
-        jmod.select()
+        pass
+      #  jmod=QSqlTableModel()
+      #  jmod.setTable("jtab")
+      #  self.ui.tableView.setModel(jmod)
+      #  jmod.select()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
