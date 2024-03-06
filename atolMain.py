46e34d511752332c1fb743e5d1e983c66ba78d26
Leonid Gnezdilov <comdir@infonix.info>
2024-03-06 15:34:01 +0300

    Update

--- atolMain.py
+++ atolMain.py
@@ -17,14 +17,14 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QComboBox, QFrame, QHBoxLayout,
     QHeaderView, QLabel, QMainWindow, QMenuBar,
-    QPushButton, QSizePolicy, QStatusBar, QTableView,
-    QVBoxLayout, QWidget)
+    QPushButton, QSizePolicy, QStatusBar, QTableWidget,
+    QTableWidgetItem, QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(585, 495)
+        MainWindow.resize(889, 495)
         sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -88,12 +88,12 @@ class Ui_MainWindow(object):
         self.frame_2.setFrameShadow(QFrame.Raised)
         self.verticalLayout_2 = QVBoxLayout(self.frame_2)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.comboBox = QComboBox(self.frame_2)
-        self.comboBox.addItem("")
-        self.comboBox.addItem("")
-        self.comboBox.setObjectName(u"comboBox")
+        self.comboBox_kassir = QComboBox(self.frame_2)
+        self.comboBox_kassir.addItem("")
+        self.comboBox_kassir.addItem("")
+        self.comboBox_kassir.setObjectName(u"comboBox_kassir")
 
-        self.verticalLayout_2.addWidget(self.comboBox)
+        self.verticalLayout_2.addWidget(self.comboBox_kassir)
 
         self.openShift = QPushButton(self.frame_2)
         self.openShift.setObjectName(u"openShift")
@@ -114,6 +114,11 @@ class Ui_MainWindow(object):
         self.pushButton_test.setObjectName(u"pushButton_test")
 
         self.verticalLayout_2.addWidget(self.pushButton_test)
+
+        self.pushButton_refresh = QPushButton(self.frame_2)
+        self.pushButton_refresh.setObjectName(u"pushButton_refresh")
+
+        self.verticalLayout_2.addWidget(self.pushButton_refresh)
 
         self.label_2 = QLabel(self.frame_2)
         self.label_2.setObjectName(u"label_2")
@@ -157,20 +162,32 @@ class Ui_MainWindow(object):
         self.frameList.setFrameShadow(QFrame.Raised)
         self.verticalLayout = QVBoxLayout(self.frameList)
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.tableView = QTableView(self.frameList)
-        self.tableView.setObjectName(u"tableView")
+        self.tableWidget = QTableWidget(self.frameList)
+        if (self.tableWidget.columnCount() < 5):
+            self.tableWidget.setColumnCount(5)
+        __qtablewidgetitem = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(0, __qtablewidgetitem)
+        __qtablewidgetitem1 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(1, __qtablewidgetitem1)
+        __qtablewidgetitem2 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(2, __qtablewidgetitem2)
+        __qtablewidgetitem3 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(3, __qtablewidgetitem3)
+        __qtablewidgetitem4 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(4, __qtablewidgetitem4)
+        self.tableWidget.setObjectName(u"tableWidget")
 
-        self.verticalLayout.addWidget(self.tableView)
+        self.verticalLayout.addWidget(self.tableWidget)
 
-        self.pushButton_2 = QPushButton(self.frameList)
-        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.pushButton_Print = QPushButton(self.frameList)
+        self.pushButton_Print.setObjectName(u"pushButton_Print")
         sizePolicy4 = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
         sizePolicy4.setHorizontalStretch(0)
         sizePolicy4.setVerticalStretch(0)
-        sizePolicy4.setHeightForWidth(self.pushButton_2.sizePolicy().hasHeightForWidth())
-        self.pushButton_2.setSizePolicy(sizePolicy4)
+        sizePolicy4.setHeightForWidth(self.pushButton_Print.sizePolicy().hasHeightForWidth())
+        self.pushButton_Print.setSizePolicy(sizePolicy4)
 
-        self.verticalLayout.addWidget(self.pushButton_2)
+        self.verticalLayout.addWidget(self.pushButton_Print)
 
 
         self.horizontalLayout.addWidget(self.frameList)
@@ -178,7 +195,7 @@ class Ui_MainWindow(object):
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 585, 30))
+        self.menubar.setGeometry(QRect(0, 0, 889, 30))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -193,16 +210,27 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"\u0421\u043c\u0435\u043d\u0430 \u0417\u0430\u043a\u0440\u044b\u0442\u0430!", None))
         self.label_3.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
-        self.comboBox.setItemText(0, QCoreApplication.translate("MainWindow", u"\u0413\u043d\u0435\u0437\u0434\u0438\u043b\u043e\u0432\u0430 \u041e.\u0418.", None))
-        self.comboBox.setItemText(1, QCoreApplication.translate("MainWindow", u"\u041d\u0435\u0447\u0435\u043f\u0443\u0440\u0435\u043d\u043a\u043e \u042d.\u0412.", None))
+        self.comboBox_kassir.setItemText(0, QCoreApplication.translate("MainWindow", u"\u0413\u043d\u0435\u0437\u0434\u0438\u043b\u043e\u0432\u0430 \u041e.\u0418.", None))
+        self.comboBox_kassir.setItemText(1, QCoreApplication.translate("MainWindow", u"\u041d\u0435\u0447\u0435\u043f\u0443\u0440\u0435\u043d\u043a\u043e \u042d.\u0412.", None))
 
         self.openShift.setText(QCoreApplication.translate("MainWindow", u"\u041e\u0442\u043a\u0440\u044b\u0442\u0438\u0435 \u0441\u043c\u0435\u043d\u044b", None))
         self.listButton.setText(QCoreApplication.translate("MainWindow", u"\u0421\u043f\u0438\u0441\u043e\u043a \u0440\u0435\u0430\u043b\u0438\u0437\u0430\u0446\u0438\u0439 =>", None))
         self.closeShift.setText(QCoreApplication.translate("MainWindow", u"\u0417\u0430\u043a\u0440\u044b\u0442\u0438\u0435 \u0441\u043c\u0435\u043d\u044b", None))
         self.pushButton_test.setText(QCoreApplication.translate("MainWindow", u"Test", None))
+        self.pushButton_refresh.setText(QCoreApplication.translate("MainWindow", u"Refresh", None))
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
         self.pushButton.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
         self.exitButton.setText(QCoreApplication.translate("MainWindow", u"\u0412\u044b\u0445\u043e\u0434", None))
-        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"\u041f\u0435\u0447\u0430\u0442\u044c \u0447\u0435\u043a\u0430", None))
+        ___qtablewidgetitem = self.tableWidget.horizontalHeaderItem(0)
+        ___qtablewidgetitem.setText(QCoreApplication.translate("MainWindow", u"\u041a\u043e\u0434 \u043d\u0430\u043a\u043b.", None));
+        ___qtablewidgetitem1 = self.tableWidget.horizontalHeaderItem(1)
+        ___qtablewidgetitem1.setText(QCoreApplication.translate("MainWindow", u"\u041d\u043e\u043c.\u043d\u0430\u043a\u043b.", None));
+        ___qtablewidgetitem2 = self.tableWidget.horizontalHeaderItem(2)
+        ___qtablewidgetitem2.setText(QCoreApplication.translate("MainWindow", u"\u0414\u0430\u0442\u0430", None));
+        ___qtablewidgetitem3 = self.tableWidget.horizontalHeaderItem(3)
+        ___qtablewidgetitem3.setText(QCoreApplication.translate("MainWindow", u"\u041a\u043e\u043d\u0442\u0440\u0430\u0433\u0435\u043d\u0442", None));
+        ___qtablewidgetitem4 = self.tableWidget.horizontalHeaderItem(4)
+        ___qtablewidgetitem4.setText(QCoreApplication.translate("MainWindow", u"\u0421\u0443\u043c\u043c\u0430", None));
+        self.pushButton_Print.setText(QCoreApplication.translate("MainWindow", u"\u041f\u0435\u0447\u0430\u0442\u044c \u0447\u0435\u043a\u0430", None))
     # retranslateUi
 
--- atolPrint.py
+++ atolPrint.py
@@ -2,7 +2,7 @@ import os
 import sys
 from time import sleep
 from PySide6.QtWidgets import QApplication, QMainWindow, QHeaderView, QTableWidget, QTableWidgetItem, QAbstractItemView, \
-    QMessageBox
+    QMessageBox, QLabel
 from atolMain import Ui_MainWindow
 from PySide6.QtSql import QSqlDatabase, QSqlTableModel, QSqlQuery
 from PySide6.QtCore import QSize
@@ -18,22 +18,62 @@ class MainWindow(QMainWindow):
     #fptr.setSingleSetting(IFptr.LIBFPTR_SETTING_PORT, str(IFptr.LIBFPTR_PORT_USB))
     fptr.setSingleSetting(IFptr.LIBFPTR_SETTING_PORT, str(IFptr.LIBFPTR_PORT_BLUETOOTH)) 
     fptr.setSingleSetting(IFptr.LIBFPTR_SETTING_MACADDRESS,'00:13:43:56:DD:7E')
-
-    #fptr.setSingleSetting(IFptr.libLIBFPTR_SETTING_COM_FILE, "COM5")
-    #fptr.setSingleSetting(IFptr.LIBFPTR_SETTING_BAUDRATE, str(IFptr.LIBFPTR_PORT_BR_115200))
     fptr.showProperties(IFptr.LIBFPTR_GUI_PARENT_QT,None)
     fptr.applySingleSettings()
-    fptr.open()
+    stattext = ""
+    if fptr.open()==0:
+        stattext="касса подключена, интерфейс открыт"
+        print ("касса подключена, интерфейс открыт")
+    else:
+        stattext="касса не найдена"
+        print ("касса не найдена")
+        
 
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        DB = QSqlDatabase.addDatabase('QSQLITE')
+        DB.setDatabaseName("susndb.ixdb")
+        DB.open()
         self.ui.pushButton_test.clicked.connect(self.test)
         self.ui.closeShift.clicked.connect(self.closeShift)
-        self.ui.listButton.clicked.connect(self.listUpdate)
+        self.ui.listButton.clicked.connect(self.formUpdate)
+        self.ui.pushButton_refresh.clicked.connect(self.refresh)
+        self.ui.pushButton_Print.clicked.connect(self.print)
+        self.statlab = QLabel()
+        self.statlab.setText(self.stattext)
+        self.statusBar().addWidget(self.statlab)
 
-    def listUpdate(self):
+    def openReceipt(self):
+        try:
+            self.fptr.closeReceipt()
+        except BaseException:
+            print (BaseException)
+
+        self.fptr.setParam(1021, "Кассир "+ self.ui.comboBox_kassir.currentText())
+        self.fptr.setParam(1203, "462500000000")
+        self.fptr.operatorLogin()
+        self.fptr.setParam(IFptr.LIBFPTR_PARAM_RECEIPT_TYPE, IFptr.LIBFPTR_RT_SELL)
+        return self.fptr.openReceipt()
+
+    def print(self):
+        if  self.openReceipt() == 0:
+            query= QSqlQuery()
+            query.exec("SELECT nomenkl, cost, kolvo, sum FROM spdetail WHERE nomsp='"+self.ui.tableWidget.item(self.ui.tableWidget.currentRow(),0).text()+"';")
+            while query.next():
+                print (str(query.value(0)))
+                self.fptr.setParam(IFptr.LIBFPTR_PARAM_COMMODITY_NAME, str(query.value(0)))
+                self.fptr.setParam(IFptr.LIBFPTR_PARAM_PRICE, int(query.value(1)))
+                self.fptr.setParam(IFptr.LIBFPTR_PARAM_QUANTITY, int(query.value(2)))
+                self.fptr.setParam(IFptr.LIBFPTR_PARAM_TAX_TYPE, IFptr.LIBFPTR_TAX_NO)
+                self.fptr.setParam(IFptr.LIBFPTR_PARAM_USE_ONLY_TAX_TYPE, True)
+                self.fptr.registration()
+            self.fptr.closeReceipt()    
+        else:
+             self.statlab.setText("Ошибка открытия чека")        
+        
+    def formUpdate(self):
         if self.ui.frameList.geometry().width() <= 300:
             self.setMaximumWidth(29005)
             self.ui.centralwidget.setMaximumWidth(65000)
@@ -53,20 +93,62 @@ class MainWindow(QMainWindow):
             #self.updateGeometry()
 
             print ("2")    
-            
+
+    def refresh(self):
+        self.updateList("SELECT kod, nom, date, kontr_name, sum FROM sprint WHERE mode='sprint';", "SELECT COUNT(*) FROM sprint WHERE mode='sprint';")
+
+    def updateList(self, que, quec):
+        query = QSqlQuery()
+        qcount = QSqlQuery()
+        qcount.exec(quec)
+        qcount.first()
+        query.exec(que)
+        #query.first()
+        r=0
+        self.ui.tableWidget.setRowCount(int(qcount.value(0)))
+        while query.next():
+            #self.ui.tableWidget.setItem(r, 0,  QTableWidgetItem(f'line_{r}'))
+            self.ui.tableWidget.setItem(r, 0,  QTableWidgetItem(str(query.value(0))))
+            self.ui.tableWidget.setItem(r, 1,  QTableWidgetItem(str(query.value(1))))
+            self.ui.tableWidget.setItem(r, 2,  QTableWidgetItem(str(query.value(2))))
+            self.ui.tableWidget.setItem(r, 3,  QTableWidgetItem(str(query.value(3))))
+            self.ui.tableWidget.setItem(r, 4,  QTableWidgetItem(str(query.value(4))))
+            # if str(query.value(7)) == "True":  # оплачено
+            #     for c in range(self.ui.tableWidget.columnCount()):
+            #         self.ui.tableWidget.item(r, c).setBackground(QColor(0, 250, 100))  # Должно поменять цвет строки
+               # pass
+            #f str(query.value(7)) == True: #выдано
+                #self.ui.tableWidget. #Должно поменять цвет строки
+            #   pass  
+            r+=1
+        #self.ui.tableWidget.resizeColumnsToContents()
+        self.ui.tableWidget.resizeColumnToContents(0)
+        self.ui.tableWidget.resizeColumnToContents(1)
+        self.ui.tableWidget.resizeColumnToContents(2)
+        self.ui.tableWidget.resizeColumnToContents(3)
+        self.ui.tableWidget.resizeColumnToContents(4)
+
+      #  self.ui.tableWidget.setColumnWidth(7, 500)
+        self.ui.tableWidget.horizontalHeader().setStretchLastSection(True)
+        self.ui.tableWidget.horizontalHeader().setSectionResizeMode(5, QHeaderView.Stretch)
+        self.ui.tableWidget.setSelectionBehavior(QAbstractItemView.SelectRows)
+
 
 
 
     def closeShift(self):
 
-        self.fptr.setParam(1021, "Кассир Иванов И.")
-        self.fptr.setParam(1203, "123456789047")
+        self.fptr.setParam(1021, "Кассир "+ self.ui.comboBox_kassir.currentText())
+        self.fptr.setParam(1203, "462500000000")
         self.fptr.operatorLogin()
-
         self.fptr.setParam(IFptr.LIBFPTR_PARAM_REPORT_TYPE, IFptr.LIBFPTR_RT_CLOSE_SHIFT)
         self.fptr.report()
+        if self.fptr.checkDocumentClosed() != 0:
+            self.statlab.setText("Ошибка, смена не закрыта")
+        else:
+            self.statlab.setText("Смена успешно закрыта")        
+            
 
-        self.fptr.checkDocumentClosed()
 
     def test(self):
 
